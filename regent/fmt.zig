55c3f8e9d0a01b2637587ac87e0e65949daa0c33
t <tattlelet@gmail.com>
2026-08-11 13:28:32 +0100

    pack/unpack handles enums and packed structs now

--- regent/fmt.zig
+++ regent/fmt.zig
@@ -18,17 +18,41 @@ pub fn floatTrunc(T: type, value: T, comptime floatPoints: u16) T {
 }
 
 pub inline fn pack(value: anytype) T: {
-    const tInfo = @typeInfo(@TypeOf(value)).@"struct";
-    if (tInfo.layout != .@"packed") @compileError("pack only supports packed structs");
-    break :T tInfo.backing_integer.?;
+    const errorM = "unpack only supports packed structs or enums";
+    const tInfo = @typeInfo(@TypeOf(value));
+    break :T switch (tInfo) {
+        .@"struct" => |sInfo| r: {
+            if (sInfo.layout != .@"packed") @compileError(errorM);
+
+            break :r sInfo.backing_integer.?;
+        },
+        .@"enum" => |eInfo| eInfo.tag_type,
+        else => @compileError(errorM),
+    };
 } {
-    return @bitCast(value);
+    return switch (@typeInfo(@TypeOf(value))) {
+        .@"struct" => @bitCast(value),
+        .@"enum" => @intFromEnum(value),
+        else => unreachable,
+    };
 }
 
 pub inline fn unpack(T: type, value: V: {
-    const tInfo = @typeInfo(T).@"struct";
-    if (tInfo.layout != .@"packed") @compileError("unpack only supports packed structs");
-    break :V tInfo.backing_integer.?;
+    const errorM = "unpack only supports packed structs or enums";
+    const tInfo = @typeInfo(T);
+    break :V switch (tInfo) {
+        .@"struct" => |sInfo| r: {
+            if (sInfo.layout != .@"packed") @compileError(errorM);
+
+            break :r sInfo.backing_integer.?;
+        },
+        .@"enum" => |eInfo| eInfo.tag_type,
+        else => @compileError(errorM),
+    };
 }) T {
-    return @bitCast(value);
+    return switch (@typeInfo(T)) {
+        .@"struct" => @bitCast(value),
+        .@"enum" => @enumFromInt(value),
+        else => unreachable,
+    };
 }
